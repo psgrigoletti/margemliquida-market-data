2b360b04c1b11c9bfb8f0991306892bd25b1c7a4
Pablo Souza Grigoletti <pablo.grigoletti@serpro.gov.br>
2024-01-19 15:28:58 -0300

    Add comentários no fundsexplorer

--- src/fundsexplorer/fundsexplorer.py
+++ src/fundsexplorer/fundsexplorer.py
@@ -1,3 +1,6 @@
+""" Módulo que trabalha com os dados do site https://www.fundsexplorer.com.br/
+"""
+
 from io import StringIO
 from time import sleep
 
@@ -6,11 +9,16 @@ from selenium_config import meu_firefox
 
 
 def buscar_dados_fundsexplorer():
+    """buscar_dados_fundsexplorer buscar dados dos FIIs no site https://www.fundsexplorer.com.br/ranking.
+
+    Returns:
+        df: DataFrame com os dados dos FIIs
+    """
     url = "https://www.fundsexplorer.com.br/ranking"
     wd = meu_firefox.configura_webdriver_firefox()
     wd.get(url)
     sleep(8)
     html_content = wd.page_source
-    fiis_fundsexplorer = pd.read_html(StringIO(str(html_content)), encoding="utf-8")[0]
-    fiis_fundsexplorer.rename(columns={"Fundos": "Código"}, inplace=True)
-    return fiis_fundsexplorer
+    df = pd.read_html(StringIO(str(html_content)), encoding="utf-8")[0]
+    df.rename(columns={"Fundos": "Código"}, inplace=True)
+    return df
