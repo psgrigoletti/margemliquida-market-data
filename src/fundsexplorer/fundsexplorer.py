ae079fac589950d17f55bb2b33cf43c6d2cfb45f
Pablo Souza Grigoletti <pablo.grigoletti@serpro.gov.br>
2024-01-19 15:37:45 -0300

    Adicionando um readme

--- src/fundsexplorer/fundsexplorer.py
+++ src/fundsexplorer/fundsexplorer.py
@@ -1,22 +1,25 @@
-""" Módulo que trabalha com os dados do site https://www.fundsexplorer.com.br/
+""" Módulo que trabalha com os dados do site https://www.fundsexplorer.com.br/.
 """
 
 from io import StringIO
 from time import sleep
 
 import pandas as pd
+
 from selenium_config import meu_firefox
+
+URL = "https://www.fundsexplorer.com.br/ranking"
 
 
 def buscar_dados_fundsexplorer():
-    """buscar_dados_fundsexplorer buscar dados dos FIIs no site https://www.fundsexplorer.com.br/ranking.
+    """*buscar_dados_fundsexplorer* buscar dados dos FIIs no site https://www.fundsexplorer.com.br/ranking.
 
     Returns:
-        df: DataFrame com os dados dos FIIs
+        df Data: DataFrame com os dados dos FIIs
     """
-    url = "https://www.fundsexplorer.com.br/ranking"
+
     wd = meu_firefox.configura_webdriver_firefox()
-    wd.get(url)
+    wd.get(URL)
     sleep(8)
     html_content = wd.page_source
     df = pd.read_html(StringIO(str(html_content)), encoding="utf-8")[0]
